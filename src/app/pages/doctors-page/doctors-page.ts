bac534c62770b90819a6d6b4f4588d3265c8909c
EdgarMolard <edgar.mlrd@gmail.com>
2025-11-18 12:56:29 +0100

    search bar a debug encore

--- src/app/pages/doctors-page/doctors-page.ts
+++ src/app/pages/doctors-page/doctors-page.ts
@@ -4,19 +4,35 @@ import { toSignal } from '@angular/core/rxjs-interop';
 import { DoctorsService } from '../../services/doctors.service';
 import { Doctor } from '../../types/doctor.interface';
 import { DoctorCard } from '../../components/doctor-card/doctor-card';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-doctors-page',  
   standalone: true,  
-  imports: [CommonModule,DoctorCard],  
+  imports: [CommonModule,DoctorCard,FormsModule],  
   templateUrl: './doctors-page.html',
   styleUrls: ['./doctors-page.css']
 })
 
 export class DoctorsPageComponent {
+
+  searchValue='';
+
   private doctorsService = inject(DoctorsService);  
   
   doctors = toSignal(this.doctorsService.getDoctors(), {
     initialValue: [] as Doctor[]
   });
+
+
+  filterDoctors(){
+    return this.doctors().filter(doctor =>
+      doctor.last_name.toLowerCase().includes(this.searchValue.toLowerCase()) ||
+      doctor.first_name.toLowerCase().includes(this.searchValue.toLowerCase()) ||
+      doctor.specialite.toLowerCase().includes(this.searchValue.toLowerCase())
+
+      );
+
+  }
+
 }
